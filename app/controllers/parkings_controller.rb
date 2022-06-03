d33f7f5683645605f695e0cd0a48229872073a81
Leandro Barbosa <leandrobarbosa@sfr.fr>
2022-06-03 08:03:48 +0200

    show#ndex

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -2,4 +2,14 @@ class ParkingsController < ApplicationController
   def index
     @parkings = Parking.all
   end
+
+  def show
+    @parking = Parking.find(params[:id])
+  end
+
+  private
+
+  def singer_params
+    params.require(:parking).permit(:title, :photo, :price, :category)
+  end
 end
