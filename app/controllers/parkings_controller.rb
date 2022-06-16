fdb5dd31831107afa7be26944fc08bfca7109c1a
lionel chamouilli <lionel.chamouilli@gmail.com>
2022-06-16 17:55:02 +0200

    Setup SearchBar on Parkings index

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,14 +1,53 @@
 class ParkingsController < ApplicationController
   def index
-    @parkings = Parking.all
-    @address_parkings = AddressParking.all
-    @markers = @address_parkings.geocoded.map do |address|
-      {
-        lat: address.latitude,
-        lng: address.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { address: address }),
-        info_marker: render_to_string(partial: "info_marker", locals: { address: address })
-      }
+    @address_parkings = []
+    if params[:search].present?
+      @parkings = Parking.search_by_address(params[:search])
+      @parkings.each do |parking|
+        parking.address_parking
+        @address_parkings << parking.address_parking
+      end
+    elsif params[:max_price].present?
+      @parkings = Parking.all
+      @parkings = @parkings.where("price <= ?", params[:max_price])
+      @parkings.each do |parking|
+        parking.address_parking
+        @address_parkings << parking.address_parking
+      end
+    else
+      @parkings = Parking.all
+      @address_parkings = AddressParking.all
+    end
+
+    if @address_parkings == AddressParking.all
+      @markers = @address_parkings.geocoded.map do |address|
+        {
+          lat: address.latitude,
+          lng: address.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { address: address }),
+          info_marker: render_to_string(partial: "info_marker", locals: { address: address })
+        }
+      end
+    elsif @address_parkings.length == 1
+      @markers = [{
+        lat: @address_parkings[0].latitude,
+        lng: @address_parkings[0].longitude,
+        info_window: render_to_string(partial: "info_window", locals: { address: @address_parkings[0] }),
+        info_marker: render_to_string(partial: "info_marker", locals: { address: @address_parkings[0] })
+      }]
+    elsif @address_parkings.is_a?(Array) && @address_parkings.length > 1
+      @markers = []
+      @address_parkings.each do |address_parking|
+        marker_informations = {
+          lat: address_parking.latitude,
+          lng: address_parking.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { address: address_parking }),
+          info_marker: render_to_string(partial: "info_marker", locals: { address: address_parking })
+        }
+        @markers << marker_informations
+      end
+    else
+      redirect_back(fallback_location: root_path, notice: "Addresse inconnue")
     end
   end
 
